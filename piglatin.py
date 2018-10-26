fd7257c24c1e6880a39ff65e892e9255fb9c4e13
Samad Syed <ssyed2018@Syed-Samad.local>
2018-10-26 15:50:21 -0400

    Your commit my solution

---
+++ piglatin.py
@@ -0,0 +1,13 @@
+
+def encode(userInput):
+	lstInput = userInput.split()
+	vowel = ("aeiouy")
+	for i in range (len(lstInput)):
+		if lstInput[i][0] in vowel:
+			lstInput[i] = lstInput[i] + "yay"
+		else:
+			for j in range(len(lstInput[i])):
+				if lstInput[i][j] in vowel:
+					lstInput[i] = lstInput[i][j:] + lstInput [i][:j] + "ay"
+					break
+	return (" ".join(lstInput)).lower()
